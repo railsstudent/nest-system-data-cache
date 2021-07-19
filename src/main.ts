69d6fde0e433bf72b48a61a74b33700ba1c8b0cf
Connie Leung <cleung334@gmail.com>
2021-07-20 00:47:44 +0800

    chore: validation pipe
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import helmet = require('helmet')
+import * as compression from 'compression'
+import * as morgan from 'morgan'
+import * as cors from 'cors'
+import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(compression())
+  app.use(cors())
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(morgan('dev'))
+  app.use(helmet())
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,3 +23,5 @@ async function bootstrap() {
   await app.listen(3000)
 }
 bootstrap()
+  .then(() => console.log('Application starts successfully'))
+  .catch(console.error)
